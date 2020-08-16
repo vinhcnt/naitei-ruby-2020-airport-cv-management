f4dd1a395be13b67caa738fba572fba9ba15e06e
Nguyen The Vinh C <nguyen.the.vinhc@sun-asterisk.com>
2020-08-16 17:17:49 +0700

    [TASK][27204] Create Static Pages

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
+const webpack = require('webpack')
 
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['default', 'popper.js']
+  })
+)
+
+environment.loaders.prepend('erb', erb)
 module.exports = environment
