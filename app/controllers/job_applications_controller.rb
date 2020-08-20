6a7d3c19dc47c51cdedd44001b335c8f97889881
Mochii <phan.thi.thuy@sun-asterisk.com>
2020-08-20 13:36:31 +0700

    [TASK][26999] Show request

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,13 +1,26 @@
 class JobApplicationsController < ApplicationController
   def index
-    @job_applications = current_user.job_applications
-                                    .desc_order
-                                    .page(params[:page])
-                                    .per Settings.job_applications.per_page
-
+    if current_user.is_recruiter?
+      @job_applications = JobApplication.desc_order
+                                        .page(params[:page])
+                                        .per Settings.job_applications.per_page
+    else
+      candidate_index
+    end
     respond_to do |format|
       format.html{render :index}
       format.js
     end
+  end
+
+  private
+
+  def candidate_index
+    return unless current_user.is_candidate?
+
+    @job_applications = current_user.job_applications
+                                    .desc_order
+                                    .page(params[:page])
+                                    .per Settings.job_applications.per_page
   end
 end
