ebc6f34f4a5dd2d3fc0170251bf8aaf4a7456d24
Nguyen The Vinh C <nguyen.the.vinhc@sun-asterisk.com>
2020-08-26 13:08:24 +0700

    [TASK][28030] Delete Profile Education

--- app/models/education.rb
+++ app/models/education.rb
@@ -2,6 +2,8 @@ class Education < ApplicationRecord
   EDUCATION_PARAMS = %i(college major date_from date_to certification additional_information profile_id).freeze
   belongs_to :profile
 
+  delegate :user, to: :profile, prefix: true, allow_nil: true
+
   validates :college, :major, presence: true,
     length: {minimum: Settings.validations.education.name.minimum,
              maximum: Settings.validations.education.name.maximum}
