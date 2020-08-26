c456dfe6d199e66761dd96e4876919323c5af284
Mochii <phan.thi.thuy@sun-asterisk.com>
2020-08-26 11:31:57 +0700

    [TASK][28033] Delete experience

--- db/migrate/20200818015343_create_job_application_statuses.rb
+++ db/migrate/20200818015343_create_job_application_statuses.rb
@@ -1,8 +1,7 @@
 class CreateJobApplicationStatuses < ActiveRecord::Migration[6.0]
   def change
     create_table :job_application_statuses do |t|
-      t.string :title
-      
+      t.string :title      
       t.timestamps
     end
   end
