9a7747df465867e747b2aa881d2c0c6281051b8d
Mochii <phan.thi.thuy@sun-asterisk.com>
2020-08-31 10:31:03 +0700

    [TASK][28470] Job Post Details

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -7,22 +7,33 @@ class JobPost < ApplicationRecord
 
   delegate :title, to: :category, prefix: true, allow_nil: true
   delegate :name, to: :unit, prefix: true, allow_nil: true
+  delegate :title, :id, to: :job_application_status, prefix: true, allow_nil: true
+  delegate :id, to: :job_post, prefix: true, allow_nil: true
 
   validates :user_id, presence: true
   validates :title, presence: true,
-    length: {minimum: Settings.validations.job_post.title_min}
+    length: {minimum: Settings.validations.job_post.title_min,
+             maximum: Settings.validations.job_post.title_max}
   validates :location, presence: true,
-    length: {minimum: Settings.validations.job_post.content_min}
-  validates :salary_from, presence: true,
-    length: {minimum: Settings.validations.job_post.number_min},
-    numericality: {greater_than: Settings.validations.job_post.zero}
-  validates :salary_to, presence: true,
-    length: {minimum: Settings.validations.job_post.number_min},
-    numericality: {greater_than: Settings.validations.job_post.zero}
+    length: {minimum: Settings.validations.job_post.content_min,
+             maximum: Settings.validations.job_post.content_max}
+  validates :salary_from, :salary_to, presence: true,
+    numericality: {greater_than: Settings.validations.job_post.min,
+                   less_than: Settings.validations.job_post.max}
   validates :description, presence: true,
-    length: {minimum: Settings.validations.job_post.content_min}
+    length: {minimum: Settings.validations.job_post.content_min,
+             maximum: Settings.validations.job_post.content_max}
   validates :requirement, presence: true,
-    length: {minimum: Settings.validations.job_post.content_min}
+    length: {minimum: Settings.validations.job_post.content_min,
+             maximum: Settings.validations.job_post.content_max}
+  validates :note, allow_nil: true,
+    length: {minimum: Settings.validations.job_post.content_min,
+             maximum: Settings.validations.job_post.content_max}
 
   scope :home_recent_jobs, ->{order(created_at: :desc).limit Settings.job}
+
+  def applied_by user
+    job_application = user.job_applications.find_by(job_post_id: id)
+    job_application.present? && job_application.job_application_status_id != Settings.status.cancel
+  end
 end
